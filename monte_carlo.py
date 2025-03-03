9f05f72c4aa2f100eba20fd0454eafc5d1fc83b8
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2025-03-03 16:57:54 +0000

    Improved plot and added "used_points" dunction
    improved the 2d case plot which shows the points inside and outside the circle.
    
    Calculated the ratio between in and outward points

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,4 +1,4 @@
-#This code is licenced with MPL 2.0 
+#This code is licenced with MPL 2.0
 """
 Created on Mon Feb 24 2025
 
@@ -7,12 +7,26 @@ Created on Mon Feb 24 2025
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from math import fsum
+ 
 class MontiCarlo:
-    
+
     def __init__(self, coords):
         "initialisation Constructor of n dimention array"
         self.coords = coords
+        
+    def used_points(self, radius):
+        
+        self.inclosed_points = np.delete(self.coords, np.where(self.coords[0]**2+self.coords[1]**2
+                                        > radius**2),axis = 1)
+        self.out_points = np.delete(self.coords, np.where(self.coords[0]**2+self.coords[1]**2
+                                        < radius**2),axis = 1)
+        
+        self.ratio = fsum( (np.where(self.coords[0]**2+self.coords[1]**2 < radius**2
+                              , 1, 0) )/ (len(self.coords[0])) )
+        
+        return
+    
     def __str__(self):
         "allows the coordinates to be printed"
         return str(self.coords)
@@ -21,7 +35,7 @@ class MontiCarlo:
         return MontiCarlo( np.add(self.coords , other.coords) )
     def __sub__(self, other):
         "subtraction of objects of same class"
-        return MontiCarlo( np.subtract(self.coords, other.coords) )  
+        return MontiCarlo( np.subtract(self.coords, other.coords) )
     def __mul__(self,other):
         "multiplication of objects of same class"
         return MontiCarlo(self.coords*other.coords)
@@ -31,7 +45,7 @@ class MontiCarlo:
     def __getitem__(self, index):
         "alllows the seperation of the coordinates"
         return self.coords[index]
-    
+
     def point_radius(self):
         "the radius position of the coordinate from the centre"
         rad_squared = np.sum(self.coords**2)
@@ -42,23 +56,52 @@ class MontiCarlo:
         lengths = 2*self.point_radius()
         area = lengths**2
         return area
+
+    def plot2d(self,radius):
+        
+        points = self.used_points(radius)
+        
+        x_square,y_square = radius,radius
+        square = [ [-x_square, -x_square, x_square, x_square, -x_square]
+                  ,[-y_square, y_square , y_square, -y_square, -y_square] ]
+
+
+        theta = np.linspace(0, 2*np.pi,100)
+        x_circ = radius*np.cos(theta)
+        y_circ = radius*np.sin(theta)
+        
+        plt.figure()
+        plt.plot(self.inclosed_points[0], self.inclosed_points[1], 'rx')
+        plt.plot(square[0],square[1],'k')
+        plt.plot(self.out_points[0], self.out_points[1], 'bx')
+        plt.plot(x_circ,y_circ,'k')
+        plt.axis('square')
+        
     # def in_circle(self, sq_boundary_lenth):
     #     "checks if the point is inside the circle of boundary"
     #     if self.point_radius() <= boundary_length.point_radius():
             
+rad = 1
+low_lim = -rad
+up_lim  = rad
+N = 10000
+x_arr =  np.random.uniform(low_lim, up_lim , size =N)
+y_arr = np.random.uniform(low_lim, up_lim , size=N)
 
-arr_2d = np.array([ 4, 4 ])
+arr_2d = np.array([x_arr, y_arr])
 test_2d = MontiCarlo(arr_2d)
 
 test_2d1 = MontiCarlo(arr_2d)
-a = test_2d**2  - test_2d1
+a = test_2d  - test_2d1
+print(a)
 b = test_2d.point_radius()
 print(b)
 
-
 t3 = test_2d.sq_boundary()
 
+test_2d.plot2d(rad)
 
+print(test_2d.ratio)
 
 
 
--- plotting.py
+++ plotting.py
@@ -10,7 +10,7 @@ import numpy as np
 
 r = 10
 x,y = r,r
-square = [ [-x, -x, x, x, -x] ,[-y, y , y, -y, -y] ]
+square = [ [-x, -x, x, x, -x] ,[-y, y , y, -y, -y] ] 
 plt.figure()
 plt.plot(square[0],square[1])
 plt.xlim(-15,15)
@@ -32,7 +32,6 @@ plt.plot([0,xr[125]],[0,yr[125]], '-->' )
 xp = [8,-4,10,1]
 yp = [7,3,9,-4]
 plt.plot(xp,yp,'gx')
-
 
 point = [6, 7]
 plt.plot(point[0],point[1],'kx')
