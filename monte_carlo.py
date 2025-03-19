4be7f85a429be4afb28382caa8c41ec5f0ee9f57
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2025-03-19 12:31:42 +0000

    pylint stuff

--- monte_carlo.py
+++ monte_carlo.py
@@ -61,18 +61,17 @@ class MonteCarlo:
         "calculates the mean, varience and standard deviation"
         self.f_array = func(self.coords)# * (self.boundary[1]-self.boundary[0])**self.dim
         self.mean = np.mean(self.f_array)
-        self.var = np.var(self.f_array)
-        self.std = np.sqrt(self.var) * (self.boundary[1]-self.boundary[0])**self.dim
-        return self.mean, self.var,self.std
+        var = np.var(self.f_array)
+        std = np.sqrt(var) * (self.boundary[1]-self.boundary[0])**self.dim
+        return self.mean, var,std
 
     def plot1d(self,func):
         "Plots the anti Derivitive of a 1D function (eg. sin(x) dx = -cos(x))"
         x_points = np.linspace(self.boundary[0], self.boundary[1],100)
         f_est =  np.empty(np.shape(x_points))
-
-        for i in range(enumerate(x_points)):
+        
+        for i in range(len(x_points)):
             samples = np.random.uniform(self.boundary[0], x_points[i], 1000)
-
             f_est[i]= (x_points[i]-self.boundary[0])*np.mean(func(samples))
 
         plt.figure()
@@ -137,12 +136,12 @@ class ParallelMonteCarlo(MonteCarlo):
                                                       self.boundaries[1],
                                                       self.n_per_rank)
 
-        self.n_coords_per_rank = len(self.points_per_rank) // dimensions
+        n_coords_per_rank = len(self.points_per_rank) // dimensions
 
-        self.coords_per_rank = self.points_per_rank[:self.n_coords_per_rank * dimensions]
-        self.coords_per_rank = self.coords_per_rank.reshape(dimensions, self.n_coords_per_rank)
+        coords_per_rank = self.points_per_rank[:n_coords_per_rank * dimensions]
+        coords_per_rank = coords_per_rank.reshape(dimensions, n_coords_per_rank)
 
-        super().__init__(self.coords_per_rank,self.boundaries)
+        super().__init__(coords_per_rank,self.boundaries)
 
 
     def parallel_integrate(self, func):
@@ -151,32 +150,32 @@ class ParallelMonteCarlo(MonteCarlo):
         local_stats = self.mean_var_std(func)
         n_total = len(self.coords)*len(self.coords[0])
 
-        self.par_integral = self. comm.reduce(local_integral, op = MPI.SUM , root = 0 )
+        par_integral = self. comm.reduce(local_integral, op = MPI.SUM , root = 0 )
 
         results = (local_integral, local_stats[0],
                        local_stats[1], local_stats[2])
 
-        self.expected_val = local_stats[0]
-        self.expected_val_squared = np.mean(self.f_array**2)
+        expected_val = local_stats[0]
+        expected_val_squared = np.mean(self.f_array**2)
 
-        self.par_expected_val = self.comm.reduce(self.expected_val,
+        par_expected_val = self.comm.reduce(expected_val,
                                                  op = MPI.SUM, root = 0)
 
-        self.par_expected_val_squared = self.comm.reduce(self.expected_val_squared,
+        par_expected_val_squared = self.comm.reduce(expected_val_squared,
                                                  op = MPI.SUM, root = 0)
 
         if self.rank == 0:
 
-            self.par_integral = self.par_integral /self.procs
+            par_integral = par_integral /self.procs
 
             boundary_dim = (self.boundaries[1] - self.boundaries[0])**self.dim
 
-            var = 1/n_total *( (self.par_expected_val_squared/self.procs)
-                                   - (self.par_expected_val/self.procs)**2 )
+            var = 1/n_total *( (par_expected_val_squared/self.procs)
+                                   - (par_expected_val/self.procs)**2 )
 
             error = np.sqrt(var) * boundary_dim
 
-            return self.par_integral, self.expected_val, var, error
+            return par_integral, expected_val, var, error
 
         return results
 
@@ -193,7 +192,7 @@ def circ(coords):
     rad_point = np.sqrt( np.sum(coords**2, axis = 0) )
     radius = 1
     rad_arr = np.where(rad_point < radius,1,0)
-    ratio = fsum(rad_arr)/len(rad_arr)
+    #ratio = fsum(rad_arr)/len(rad_arr)
     return rad_arr
 
 
@@ -231,23 +230,22 @@ if __name__ == "__main__":
     ###########################################################################
     #Testing for non parallel computations
     ###########################################################################
-    # print('\n1D testing')
-    # arr_1d = np.array([x_arr])
-    # test_1d = MonteCarlo(arr_1d, bounds)
-    # I =test_1d.integrate(sin)
-    # test_1d.plot1d(sin)
-    # print(f'integral check = {I} \nmean, var & std = {test_1d.mean_var_std(sin)}')
+    print('\n1D testing')
+    arr_1d = np.array([x_arr])
+    test_1d = MonteCarlo(arr_1d, bounds)
+    I =test_1d.integrate(sin)
+    test_1d.plot1d(sin)
+    print(f'integral check = {I} \nmean, var & std = {test_1d.mean_var_std(sin)}')
 
-    # print('\n2D testing')
-    # arr_2d = np.array([x_arr, y_arr])
-    # test_2d = MonteCarlo(arr_2d, bounds)
+    print('\n2D testing')
+    arr_2d = np.array([x_arr, y_arr])
+    test_2d = MonteCarlo(arr_2d, bounds)
 
-    # test_2d1 = MonteCarlo(arr_2d, bounds)
-    # a = test_2d  - test_2d1
-    # test_2d.plotcirc()
-    # print(f'integral check = {test_2d.integrate(circ)}')
-    # print(f'ratio = {test_2d.ratio}, pi = {test_2d.ratio*4}')
-    # print(f'mean,var & std = {test_2d.mean_var_std(circ)}')
+    test_2d1 = MonteCarlo(arr_2d, bounds)
+    test_2d.plotcirc()
+    print(f'integral check = {test_2d.integrate(circ)}')
+    print(f'ratio = {test_2d.ratio}, pi = {test_2d.ratio*4}')
+    print(f'mean,var & std = {test_2d.mean_var_std(circ)}')
 
     ###########################################################################
     #Testing for Parallel Computations
