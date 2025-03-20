3bd9e198db1818670e34db815c63b70add555afd
JacKenzie210 <jack.mackenzie.2020@uni.strath.ac.uk>
2025-03-20 21:05:30 +0000

    Made some UI changes
    pylint improvements, improved prints for nicer output, removed redundant code such as "ratio"

--- monte_carlo.py
+++ monte_carlo.py
@@ -32,7 +32,6 @@ class MonteCarlo:
         self.boundary = boundary
         self.dim = len(coords[:,0])
 
-
     def __str__(self):
         "allows the coordinates to be printed"
         return str(self.coords)
@@ -40,7 +39,6 @@ class MonteCarlo:
     def __getitem__(self, index):
         "alllows the seperation of the coordinates"
         return self.coords[index]
-
 
 
     def integrate(self, func):
@@ -65,12 +63,40 @@ class MonteCarlo:
         std = np.sqrt(var) * (self.boundary[1]-self.boundary[0])**self.dim
         return self.mean, var,std
 
+
+    def plotcirc(self):
+        "Plots the special case of the dart board example"
+        radius = (abs(self.boundary[0])+abs(self.boundary[1]))/2
+
+        inclosed_points = np.delete(self.coords, np.where(self.coords[0]**2+
+                                    self.coords[1]**2 > radius**2),axis = 1)
+
+        out_points = np.delete(self.coords, np.where(self.coords[0]**2+
+                                    self.coords[1]**2 < radius**2),axis = 1)
+
+        x_square,y_square = radius,radius
+        square = [ [-x_square, -x_square, x_square, x_square, -x_square]
+                  ,[-y_square, y_square , y_square, -y_square, -y_square] ]
+
+
+        theta = np.linspace(0, 2*np.pi,100)
+        x_circ = radius*np.cos(theta)
+        y_circ = radius*np.sin(theta)
+
+        plt.figure()
+        plt.plot(inclosed_points[0], inclosed_points[1], 'rx')
+        plt.plot(square[0],square[1],'k')
+        plt.plot(out_points[0], out_points[1], 'bx')
+        plt.plot(x_circ,y_circ,'k')
+        plt.axis('square')
+        plt.show()
+
     def plot1d(self,func):
         "Plots the anti Derivitive of a 1D function (eg. sin(x) dx = -cos(x))"
         x_points = np.linspace(self.boundary[0], self.boundary[1],100)
         f_est =  np.empty(np.shape(x_points))
 
-        for i in range(len(x_points)):
+        for i in range(enumerate(x_points)):
             samples = np.random.uniform(self.boundary[0], x_points[i], 1000)
             f_est[i]= (x_points[i]-self.boundary[0])*np.mean(func(samples))
 
@@ -79,38 +105,6 @@ class MonteCarlo:
         plt.xlabel('x points')
         plt.title('Anti Derivitive of F(x)')
         plt.show()
-
-
-    def circ_points(self):
-        "Used to calculate the ratio of points inside and outside the circle"
-        self.radius = (abs(self.boundary[0])+abs(self.boundary[1]))/2
-
-        self.inclosed_points = np.delete(self.coords, np.where(self.coords[0]**2+self.coords[1]**2
-                                        > self.radius**2),axis = 1)
-        self.out_points = np.delete(self.coords, np.where(self.coords[0]**2+self.coords[1]**2
-                                        < self.radius**2),axis = 1)
-
-        self.ratio =  fsum( (np.where(self.coords[0]**2 +self.coords[1]**2< self.radius**2
-                              , 1, 0) )/ (len(self.coords[0]))  )
-
-    def plotcirc(self):
-        "Plots the special case of the dart board example"
-        self.circ_points()
-        x_square,y_square = self.radius,self.radius
-        square = [ [-x_square, -x_square, x_square, x_square, -x_square]
-                  ,[-y_square, y_square , y_square, -y_square, -y_square] ]
-
-
-        theta = np.linspace(0, 2*np.pi,100)
-        x_circ = self.radius*np.cos(theta)
-        y_circ = self.radius*np.sin(theta)
-
-        plt.figure()
-        plt.plot(self.inclosed_points[0], self.inclosed_points[1], 'rx')
-        plt.plot(square[0],square[1],'k')
-        plt.plot(self.out_points[0], self.out_points[1], 'bx')
-        plt.plot(x_circ,y_circ,'k')
-        plt.axis('square')
 
 
 
@@ -126,34 +120,31 @@ class ParallelMonteCarlo(MonteCarlo):
 
 
         self.procs = self.comm.Get_size()
-        self.n_per_rank = n_per_rank
-        self.total_points = self.n_per_rank*self.procs
 
-        self.boundaries = boundaries
-        self.points_per_rank  = np.random.uniform(self.boundaries[0],
-                                                      self.boundaries[1],
-                                                      self.n_per_rank)
+        self.total_points = n_per_rank*self.procs
+
+
+        self.points_per_rank  = np.random.uniform(boundaries[0],
+                                                      boundaries[1],
+                                                      n_per_rank)
 
         n_coords_per_rank = len(self.points_per_rank) // dimensions
         coords_per_rank = self.points_per_rank[:n_coords_per_rank * dimensions]
         coords_per_rank = coords_per_rank.reshape(dimensions, n_coords_per_rank)
+        
 
-        super().__init__(coords_per_rank,self.boundaries)
+        super().__init__(coords_per_rank,boundaries)
 
 
     def parallel_integrate(self, func):
         "enables each rank to integral with the mean,varience and error(std)"
         local_integral = self.integrate(func)
-        
+
         local_stats = self.mean_var_std(func)
 
         n_total = len(self.coords)*len(self.coords[0])
 
         par_integral = self.comm.reduce(local_integral, op = MPI.SUM , root = 0 )
-
-        results = (local_integral, local_stats[0],
-                       local_stats[1], local_stats[2])
-
 
         expected_val = local_stats[0]
         expected_val_squared = np.mean(self.f_array**2)
@@ -168,28 +159,23 @@ class ParallelMonteCarlo(MonteCarlo):
 
             par_integral = par_integral /self.procs
 
-            boundary_dim = (self.boundaries[1] - self.boundaries[0])**self.dim
+            boundary_dim = (self.boundary[1] - self.boundary[0])**self.dim
 
             var = 1/n_total *( (par_expected_val_squared/self.procs)
                                    - (par_expected_val/self.procs)**2 )
 
             error = np.sqrt(var) * boundary_dim
-            
-            print('\nRank 0 - reduced MPI.SUM result\n-----------------------',
-                  f'\n{self.dim} dimentional {func.__name__}',
+
+            print(f'\n{self.dim} dimentional {func.__name__}',
+                  '\n-------------------------',
                   f'\nIntegral = {par_integral}',
                   f'\nMean = {expected_val}',
                   f'\nVar = {var}',
                   f'\nStd = {error}')
 
             return par_integral, expected_val, var, error
-        
-        
-        
-        
 
-        return results
-
+        return None
 
 
 
@@ -203,7 +189,6 @@ def circ(coords):
     rad_point = np.sqrt( np.sum(coords**2, axis = 0) )
     radius = 1
     rad_arr = np.where(rad_point < radius,1,0)
-    #ratio = fsum(rad_arr)/len(rad_arr)
     return rad_arr
 
 
@@ -233,19 +218,24 @@ if __name__ == "__main__":
     ###########################################################################
     #Initial Conditions
     ###########################################################################
-    
+
     LOW_LIM = -1
     UP_LIM  = 1
     bounds = np.array([LOW_LIM,UP_LIM])
 
     NUM_PER_RANK = 1000000
     N_DIM = 6
+    N_COORDS = NUM_PER_RANK // N_DIM
+
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        print(f' {NUM_PER_RANK} points per rank in {N_DIM} dimentions gives',
+              f' \n{N_COORDS} coordiantes per rank')
+
 
     ###########################################################################
     #Task 1
     ###########################################################################
 
-    'Repeats integral calulation for each Dimention'
     for dim in range(1,N_DIM+1):
         par_circ = ParallelMonteCarlo(NUM_PER_RANK, bounds, dim)
         par_circ_integral = par_circ.parallel_integrate(circ)
@@ -254,11 +244,9 @@ if __name__ == "__main__":
     ###########################################################################
     #Task 2
     ###########################################################################
-    'Repeats integral calulation for each Dimention'
+
     for dim in range(1,N_DIM+1):
         par_guass = ParallelMonteCarlo(NUM_PER_RANK, bounds, dim)
         par_guass_integral = par_guass.parallel_integrate(gaussian)
-
-
 
     ###########################################################################
